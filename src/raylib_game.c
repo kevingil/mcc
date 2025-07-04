1c292f0ab9eedaf9f530324a602990b689d23a0e
Kevin Gil <kevin@sellscale.com>
2025-07-04 00:41:01 -0700

    update scren size

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -29,8 +29,8 @@ Sound fxCoin = { 0 };
 //----------------------------------------------------------------------------------
 // Local Variables Definition (local to this module)
 //----------------------------------------------------------------------------------
-static const int screenWidth = 800;
-static const int screenHeight = 450;
+static const int screenWidth = 1280;
+static const int screenHeight = 720;
 
 // Required variables to manage screen transitions (fade-in, fade-out)
 static float transAlpha = 0.0f;
